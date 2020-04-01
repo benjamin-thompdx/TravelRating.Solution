74110aa55fb907ef7236034ca4d81bc091a35d25
Benjamin Thom <jaminthom@gmail.com>
2020-04-01 11:18:39 -0700

    update TravelRating/Startup.cs
    Co-authored-by: Matt <taylor.matt@protonmail.com>
    Co-authored-by: K. Wicz <katy@solovewicz.com>
    Co-authored-by: Steph Podolak <StephPodolak@gmail.com>

--- TravelRating/Startup.cs
+++ TravelRating/Startup.cs
@@ -5,6 +5,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TravelRating.Helpers;
+using TravelRating.Services;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 
 namespace TravelRating
 {
@@ -12,7 +18,7 @@ namespace TravelRating
     {
       public Startup(IConfiguration configuration)
       {
-          Configuration = configuration;
+        Configuration = configuration;
       }
 
       public IConfiguration Configuration { get; }
@@ -20,42 +26,78 @@ namespace TravelRating
       // This method gets called by the runtime. Use this method to add services to the container.
       public void ConfigureServices(IServiceCollection services)
       {
-          services.AddDbContext<TravelRatingContext>(opt =>
-              opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
-          
-          services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+				services.AddDbContext<TravelRatingContext>(opt =>
+						opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+				
+				services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+				
+				services.AddCors();
 
-            // services.AddIdentity<ApplicationUser, IdentityRole>()
-            //   .AddEntityFrameworkStores<TravelRatingContext>()
-            //   .AddDefaultTokenProviders();
+				var appSettingsSection = Configuration.GetSection("AppSettings");
+					services.Configure<AppSettings>(appSettingsSection);
 
-            // services.Configure<IdentityOptions>(options =>
-            // {
-            //   options.Password.RequireDigit = false;
-            //   options.Password.RequiredLength = 0;
-            //   options.Password.RequireLowercase = false;
-            //   options.Password.RequireNonAlphanumeric = false;
-            //   options.Password.RequireUppercase = false;
-            //   options.Password.RequiredUniqueChars = 0;
-            // });         
+				// configure jwt authentication
+				var appSettings = appSettingsSection.Get<AppSettings>();
+				var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+				services.AddAuthentication(x =>
+				{
+					x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+					x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+				})
+				.AddJwtBearer(x =>
+				{
+					x.RequireHttpsMetadata = false;
+					x.SaveToken = true;
+					x.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidateIssuerSigningKey = true,
+						IssuerSigningKey = new SymmetricSecurityKey(key),
+						ValidateIssuer = false,
+						ValidateAudience = false
+					};
+				});
+
+				// configure DI for application services
+				services.AddScoped<IUserService, UserService>();
+
+				// services.AddIdentity<ApplicationUser, IdentityRole>()
+				//   .AddEntityFrameworkStores<TravelRatingContext>()
+				//   .AddDefaultTokenProviders();
+
+				// services.Configure<IdentityOptions>(options =>
+				// {
+				//   options.Password.RequireDigit = false;
+				//   options.Password.RequiredLength = 0;
+				//   options.Password.RequireLowercase = false;
+				//   options.Password.RequireNonAlphanumeric = false;
+				//   options.Password.RequireUppercase = false;
+				//   options.Password.RequiredUniqueChars = 0;
+				// });         
 
       }
 
       // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
       public void Configure(IApplicationBuilder app, IHostingEnvironment env)
       {
-          if (env.IsDevelopment())
-          {
-              app.UseDeveloperExceptionPage();
-          }
-          else
-          {
-              // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-              app.UseHsts();
-          }
+				if (env.IsDevelopment())
+				{
+					app.UseDeveloperExceptionPage();
+				}
+				else
+				{
+					// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+					app.UseHsts();
+				}
 
-          // app.UseHttpsRedirection();
-          app.UseMvc();
-        }
+				// global cors policy
+				app.UseCors(x => x
+						.AllowAnyOrigin()
+						.AllowAnyMethod()
+						.AllowAnyHeader());
+
+				app.UseAuthentication();
+
+				app.UseMvc();
+			}
     }
 }
