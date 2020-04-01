497b941d4d6d007ec44b3c75e6269f5b215f8a4d
Benjamin Thom <jaminthom@gmail.com>
2020-04-01 10:28:50 -0700

    add ReseedDatabase migration
    Co-authored-by: Matt <taylor.matt@protonmail.com>
    Co-authored-by: K. Wicz <katy@solovewicz.com>
    Co-authored-by: Steph Podolak <StephPodolak@gmail.com>

---
+++ TravelRating/Migrations/20200401163336_ReseedDatabase.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelRating.Migrations
+{
+    public partial class ReseedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Experiences",
+                keyColumn: "ExperienceId",
+                keyValue: 1,
+                column: "Author",
+                value: "Ben");
+
+            migrationBuilder.UpdateData(
+                table: "Experiences",
+                keyColumn: "ExperienceId",
+                keyValue: 2,
+                column: "Author",
+                value: "Matt");
+
+            migrationBuilder.UpdateData(
+                table: "Experiences",
+                keyColumn: "ExperienceId",
+                keyValue: 3,
+                column: "Author",
+                value: "Katy");
+
+            migrationBuilder.UpdateData(
+                table: "Experiences",
+                keyColumn: "ExperienceId",
+                keyValue: 4,
+                column: "Author",
+                value: "Steph");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Experiences",
+                keyColumn: "ExperienceId",
+                keyValue: 1,
+                column: "Author",
+                value: "Ben's Mom");
+
+            migrationBuilder.UpdateData(
+                table: "Experiences",
+                keyColumn: "ExperienceId",
+                keyValue: 2,
+                column: "Author",
+                value: "Matt's Mom");
+
+            migrationBuilder.UpdateData(
+                table: "Experiences",
+                keyColumn: "ExperienceId",
+                keyValue: 3,
+                column: "Author",
+                value: "Katy's Mom");
+
+            migrationBuilder.UpdateData(
+                table: "Experiences",
+                keyColumn: "ExperienceId",
+                keyValue: 4,
+                column: "Author",
+                value: "Katy's Kids' Mom");
+        }
+    }
+}
